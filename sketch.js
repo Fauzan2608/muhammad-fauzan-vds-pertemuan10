515702ada7ee441d2b0e292755e968c9acb15083
Fauzan2608 <151977866+Fauzan2608@users.noreply.github.com>
2024-04-25 09:51:24 +0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+let P = []; 
+let t = [];
+let r; //materi kelas: input user
+let k; //tugas : r dan k input user
+let P0 = 20;
+let dt = 0.1;
+let tmax = 10;
+
+let grafik;
+
+function setup() {
+  createCanvas(400, 400);
+  
+  r = createInput(400, 400);
+  r.position()
+}
+
+function draw() {
+  background(220);
+}
